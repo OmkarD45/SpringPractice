56731a22bb49e3aa6efad34752595beaa974fd34
Om <omkardeshmukh162001@gmail.com>
2023-09-15 00:10:42 +0530

    Constructor Injection 15092023

--- src/main/java/com/springcore/ci/Addition.java
+++ src/main/java/com/springcore/ci/Addition.java
@@ -12,7 +12,11 @@ public class Addition {
 		this.b=b;
 		System.out.println("Constructor : int, int");
 	}
-	
+	public Addition(String a,String b) {
+		this.a=Integer.parseInt(a);
+		this.b=Integer.parseInt(b);
+		System.out.println("Constructor : String, String");
+	}
 	public void doSum() {
 		System.out.println("Sum is: "+(this.a+this.b));
 	}
